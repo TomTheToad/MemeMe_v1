23cae57d13ff0452538cc0abf3a36640fb1b7c26
TomTheToad <tomthetoad951@gmail.com>
2015-12-31 14:17:09 -0700

    Added size to fit programmatically. This was already set in the storyboard yet the application of default attributes could possibly wipe this setting.

--- MemeMe/TextFieldDelegate.swift
+++ MemeMe/TextFieldDelegate.swift
@@ -14,6 +14,7 @@ class TextFieldDelegate: NSObject, UITextFieldDelegate {
         textField.clearsOnBeginEditing = true
         textField.placeholder = nil
         textField.textAlignment = .Center
+        textField.sizeToFit()
         
     }
     
